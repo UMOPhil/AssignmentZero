a9263d839dc64f81dc68d7d07d40914c38381f90
UMOPhil <philip.ramos@maine.edu>
2026-09-03 09:22:31 -0400

    AssignmentZero

--- AssignmentZero.java
+++ AssignmentZero.java
@@ -1,4 +1,4 @@
-public class Test {
+public class AssignmentZero {
     public static void main(String[] args){
         System.out.println("Hello Universe");
         System.out.println("Phil Ramos");
